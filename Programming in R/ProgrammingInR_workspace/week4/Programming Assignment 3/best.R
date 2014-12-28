a69fcf288d9ef35a0a911451e585c7e057fdc0a3
GitOutATown <aqumegeemail@gmail.com>
2014-12-27 19:32:07 -0800

    Initial commit for week 4 programming assignment 3.

---
+++ Programming in R/ProgrammingInR_workspace/week4/Programming Assignment 3/best.R
@@ -0,0 +1,27 @@
+## best.R
+
+best <- function(state, outcome) {
+    ## Read outcome data
+    dataTable <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
+    
+    ## Check that state is valid
+    stateNames <- unique(dataTable$State)
+    validState <- stateNames[stateNames==state]
+    if(!length(validState) > 0) stop("invalid state")
+    
+    ## Check that outcome valid
+    outcomeArgs <- c("heart attack", "heart failure", "pneumonia")
+    validOutcomeName <- outcomeArgs[outcomeArgs==outcome]
+    if(!length(validOutcomeName) > 0) stop("invalid outcome")
+    
+    ## Get relevant data records
+    outcomeStem <- "Hospital.30.Day.Death..Mortality..Rates.from."
+    colNames <- c(paste(outcomeStem, "Heart.Attack", sep=""),
+                  paste(outcomeStem, "Heart.Failure", sep=""),
+                  paste(outcomeStem, "Pneumonia", sep=""))
+    names(colNames) <- outcomeArgs       
+    data <- dataTable[dataTable$State==state, c("Hospital.Name", colNames[outcome])]
+    
+    ## Return hospital name in that state with lowest 30-day death rate
+    
+}
