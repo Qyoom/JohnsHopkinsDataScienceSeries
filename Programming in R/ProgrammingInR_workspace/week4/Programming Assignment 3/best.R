85945a7f0e29bfcdca89b580d81f96fe4882085d
GitOutATown <aqumegeemail@gmail.com>
2014-12-28 11:49:24 -0800

    Completed problem 1 of week 4 programming assignments.

--- Programming in R/ProgrammingInR_workspace/week4/Programming Assignment 3/best.R
+++ Programming in R/ProgrammingInR_workspace/week4/Programming Assignment 3/best.R
@@ -3,13 +3,13 @@
 best <- function(state, outcome) {
     ## Read outcome data
     dataTable <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
-    
+        
     ## Check that state is valid
     stateNames <- unique(dataTable$State)
     validState <- stateNames[stateNames==state]
     if(!length(validState) > 0) stop("invalid state")
     
-    ## Check that outcome valid
+    ## Check that outcome is valid
     outcomeArgs <- c("heart attack", "heart failure", "pneumonia")
     validOutcomeName <- outcomeArgs[outcomeArgs==outcome]
     if(!length(validOutcomeName) > 0) stop("invalid outcome")
@@ -19,14 +19,22 @@ best <- function(state, outcome) {
     colNames <- c(paste(outcomeStem, "Heart.Attack", sep=""),
                   paste(outcomeStem, "Heart.Failure", sep=""),
                   paste(outcomeStem, "Pneumonia", sep=""))
-    names(colNames) <- outcomeArgs       
-    data <- dataTable[dataTable$State==state, c("Hospital.Name", colNames[outcome])]
-    dataNotNA <- data[data[colNames[outcome]] != "Not Available", c("Hospital.Name", colNames[outcome])]
-    
+    names(colNames) <- outcomeArgs  
+    outcomeCol <- colNames[outcome]
+    data <- dataTable[dataTable$State==state, c("Hospital.Name", outcomeCol)]
+    dataNotNA <- data[data[outcomeCol] != "Not Available", c("Hospital.Name", outcomeCol)]
+        
     ## Return hospital name in that state with lowest 30-day death rate
-    minOutcome <- min(as.numeric(dataNotNA[,2]))
-    bestHospital <- dataNotNA[dataNotNA[,2]==minOutcome,]
-    
-    
-    
+    orderedOutcomeIndex <- order(as.numeric(dataNotNA[,2]))
+    orderedByOutcome <- dataNotNA[orderedOutcomeIndex,]
+    bestOutcome <- orderedByOutcome[1,2]
+    bestHospital <- orderedByOutcome[orderedByOutcome[,2] == bestOutcome, 1]
+    if(length(bestHospital) > 1) {
+        sortedByAlpha <- sort(bestHospital)
+        bestHospital <- sortedByAlpha[1]
+    }
+    bestHospital
 }
+
+
+
