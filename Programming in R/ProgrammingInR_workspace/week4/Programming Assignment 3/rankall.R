e90d977b4797674db7296b478f8ead33a7684d01
GitOutATown <aqumegeemail@gmail.com>
2014-12-28 18:31:09 -0800

    Completion of week 4 assignment.

--- Programming in R/ProgrammingInR_workspace/week4/Programming Assignment 3/rankall.R
+++ Programming in R/ProgrammingInR_workspace/week4/Programming Assignment 3/rankall.R
@@ -1,4 +1,6 @@
 rankall <- function(outcome, num = "best") {
+    targetsAllStates <- matrix(, nrow = 0, ncol = 2)
+    
     ## Read outcome data
     dataTable <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
     
@@ -18,4 +20,43 @@ rankall <- function(outcome, num = "best") {
     names(colNames) <- outcomeArgs  
     outcomeCol <- colNames[outcome]
     
+    ## For each state, produce a vector with the 
+    ## hospital names and the (abbreviated) state name
+    for(state in stateNames) {
+        targetHospital <- "NA"
+        data <- dataTable[dataTable$State==state, c("Hospital.Name", outcomeCol)]
+        dataNotNA <- data[data[outcomeCol] != "Not Available", c("Hospital.Name", outcomeCol)]
+        
+        ## Sort records by outcome
+        orderedOutcomeIndex <- order(as.numeric(dataNotNA[,2]), dataNotNA[,1])
+        orderedByOutcome <- dataNotNA[orderedOutcomeIndex,]
+        
+        ## Determine and validate num argument
+        # Select hospital by rank
+        if(class(num) == "numeric") {
+            if(length(orderedByOutcome) <= num && nrow(orderedByOutcome) > 0) {
+                targetHospital <- orderedByOutcome[num, 1]
+            }
+        }
+        # Select best or worst hospital
+        else if(class(num) == "character") {
+            if(num == "best") {
+                targetHospital <- orderedByOutcome[1, 1]
+            }
+            else if(num == "worst") {
+                targetHospital <- orderedByOutcome[nrow(orderedByOutcome), 1]
+            }
+        }
+        
+        ## Assemble matrix
+        targetsAllStates <- rbind(targetsAllStates, c(targetHospital, state))
+    } # END for(state in stateNames)
+    
+    ## Convert matrix to data.frame
+    df <- as.data.frame(targetsAllStates)
+    colnames(df) <- c("hospital", "state")
+    df # return
 }
+
+
+
